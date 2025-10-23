8dc4689e8a22e7710552738ef0d6498ab8eb405c
Nilesh Garg <thenileshgarg001@gmail.com>
2025-10-23 14:51:38 -0400

    Created the main agent logic

--- src/agent.py
+++ src/agent.py
@@ -1,5 +1,16 @@
-from tools import get_weather_for_location, get_user_location, Context
+from langchain.agents import create_agent
+from tools import get_user_location, get_weather_for_location, Context
+from schema import ResponseFormat
 from system_prompt import SYSTEM_PROMPT
 from model import model
-from schema import ResponseFormat
 from memory import checkpointer
+
+def build_agent():
+    return create_agent(
+        model=model,
+        system_prompt=SYSTEM_PROMPT,
+        tools=[get_user_location, get_weather_for_location],
+        context_schema=Context,
+        response_format=ResponseFormat,
+        checkpointer=checkpointer,
+    )
